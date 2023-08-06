06ff384fa72e70bf978b084aa3e5eb797a826564
Overloque <poludnicyn.99@mail.ru>
2023-08-07 02:25:16 +0500

    new_branch_2 implement method

--- src/test/java/demo/qa/TestConflictGit.java
+++ src/test/java/demo/qa/TestConflictGit.java
@@ -1,11 +1,10 @@
 package demo.qa;
 
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.*;
 
 public class TestConflictGit {
     @Test
     void openChrome () {
-        open("https://www.google.com");
+        // OPEN CHROME
     }
 }
